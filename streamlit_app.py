68f57daebed60cae0d0282627168181dbc306e91
Ankit Sahu <108117695+Ankito45@users.noreply.github.com>
2026-04-29 15:10:17 +0530

    Uncomment SQL insert statement for orders

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,8 +49,8 @@ if ingredients_list:
         
     # st.write(ingredients_list)
 
-    # my_insert_stmt = """ insert into SMOOTHIES.PUBLIC.ORDERS(INGREDIENTS, NAME_ON_ORDER)values 
-    #                 ('""" + ingredients_string + """','"""+name_on_order+"""')"""
+    my_insert_stmt = """ insert into SMOOTHIES.PUBLIC.ORDERS(INGREDIENTS, NAME_ON_ORDER)values 
+                    ('""" + ingredients_string + """','"""+name_on_order+"""')"""
     # st.write(my_insert_stmt)
   
     time_to_insert = st.button('Submit Order')
