122e1c4dff3e7472e078d4a2838d3653e4956dca
Ankit Sahu <108117695+Ankito45@users.noreply.github.com>
2026-04-29 13:39:54 +0530

    Implement smoothie order submission and nutrition display
    Added functionality to submit smoothie orders and display nutrition information.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests  
+import pandas as pd
 
 
 # Write directly to the app
@@ -27,35 +28,23 @@ ingredients_list = st.multiselect(
     max_selections = 5
 )
 
+# New Section to display smoothiefront nutrition information
 if ingredients_list:
     # st.write(ingredients_list)
     # st.text(ingredients_list)
-    
     ingredients_string = ''
   
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen
-    # st.write(ingredients_list)
-
-    my_insert_stmt = """ insert into SMOOTHIES.PUBLIC.ORDERS(INGREDIENTS, NAME_ON_ORDER)values 
-                    ('""" + ingredients_string + """','"""+name_on_order+"""')"""
-
-    st.write(my_insert_stmt)
-    time_to_insert = st.button('Submit Order')
-
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
-        st.success('Your Smoothie is ordered!', icon="✅")
-
-# New Section to display smoothiefront nutrition information
-if ingredients_list:
-    ingredients_string = ''
-  
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ''
+        iingredients_string += fruit_chosen + ''
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/{search_on}")
         sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+    time_to_insert = st.button('Submit Order')
+
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered!', icon="✅")
+
