7658b106cae2fa518707f404fa1b746ac95181c1
Ankit Sahu <108117695+Ankito45@users.noreply.github.com>
2026-04-29 14:54:22 +0530

    Modify smoothie options and comment out SQL insert
    Updated the smoothie order functionality to include an additional column 'SEARCH_ON' from the fruit options table. Commented out the SQL insert statement for future use.

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,8 +17,8 @@ st.write('The name on your smoothies will be:', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
@@ -33,18 +33,18 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen
+        st.subheader(fruit_chosen  + 'Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)  
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
         
     # st.write(ingredients_list)
 
-    my_insert_stmt = """ insert into SMOOTHIES.PUBLIC.ORDERS(INGREDIENTS, NAME_ON_ORDER)values 
-                    ('""" + ingredients_string + """','"""+name_on_order+"""')"""
-
-    st.write(my_insert_stmt)
+    # my_insert_stmt = """ insert into SMOOTHIES.PUBLIC.ORDERS(INGREDIENTS, NAME_ON_ORDER)values 
+    #                 ('""" + ingredients_string + """','"""+name_on_order+"""')"""
+    # st.write(my_insert_stmt)
+  
     time_to_insert = st.button('Submit Order')
 
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
-      
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response)
