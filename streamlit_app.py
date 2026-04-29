8dc9ed0610eddd52665746e6735a4d58ce3b668d
Ankit Sahu <108117695+Ankito45@users.noreply.github.com>
2026-04-29 13:08:59 +0530

    Update title for smoothie app to healthy dinner
    Changed Title for the application

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
-st.title(f" 🥤Customise Your Smothie 🥤")
+st.title(f"🍅 My Parents new Healthy Dinner 🍅")
 st.write(
   """
   Choose the fruits you want in your custom Smoothie
