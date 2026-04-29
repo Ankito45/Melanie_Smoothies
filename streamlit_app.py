84250a6cea9531dc6a49f8f4104910ff118fcbb5
Ankit Sahu <108117695+Ankito45@users.noreply.github.com>
2026-04-29 13:19:30 +0530

    Change title to 'Customise Your Smoothie'

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
-st.title(f"🍅 My Parents new Healthy Dinner 🍅")
+st.title(f"🥤 Customise Your Smothie 🥤")
 st.write(
   """
   Choose the fruits you want in your custom Smoothie
